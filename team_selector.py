4c39aecc3df8fd7d1c3d6c6f72cc165fe9f03358
Austin W. <147464164+CrimsonX49@users.noreply.github.com>
2024-02-19 21:49:11 -0500

    - Completed proto Team 3.

--- team_selector.py
+++ team_selector.py
@@ -16,16 +16,23 @@ players = ["Martin", "Craig", "Sue",
 
 random.shuffle(players)
     
-team1 = players[:len(players)//2]
+team1 = players[:len(players)//3]
 print("Team 1 Captain: " + random.choice(team1))
 print("Team 1:")
 for player in team1:
     print(player, end=" ")
 print("\n")
 
-team2 = players[len(players)//2:]
+team2 = players[len(players)//3:(len(players))//3*2]
 print("Team 2 Captain: " + random.choice(team2))
 print("Team 2:")
 for player in team2:
+    print(player, end=" ")
+print("\n")
+
+team3 = players[(len(players))//3*2:]
+print("Team 3 Captain: " + random.choice(team3))
+print("Team 3:")
+for player in team3:
     print(player, end=" ")
 print("\n")
